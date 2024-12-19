b5564916445f6eb24aa19490e3c904ce2e9fffe6
AdamHassouni <adamhassouni111@gmail.com>
2024-12-19 20:33:21 +0100

    update2

---
+++ settings.py
@@ -0,0 +1,11 @@
+KAFKA_BOOTSTRAP_SERVERS = ["localhost:9092"]
+SUBREDDITS = ["wallstreetbets", "stocks", "investing", "finance", "personalfinance"]
+RAW_TOPIC = "reddit_raw"
+CLEAN_TOPIC = "reddit_clean"
+SENTIMENT_TOPIC = "reddit_sentiment"
+DB_CONNECTION = "postgresql://user:password@localhost:5432/reddit_sentiment_db"
+REDDIT_CLIENT_ID = "z35mbY1mFhSd63XRWUkMZg"
+REDDIT_CLIENT_SECRET = "s3a5tkRm5gpJUtNzqleUzmrzbP-4vQ"
+REDDIT_USERNAME = "Sea-Consequence912"
+REDDIT_PASSWORD = "Hurlement030198-"
+REDDIT_USER_AGENT = "testscript by u/Sea-Consequence912"
